e20239d5da8540a894f20ecf6d299286a7765901
sldstrst <bog7673@gmail.com>
2021-04-26 19:40:42 +0300

    Create Correct.java
    Correct.java checks the correctness of the data (person)

---
+++ Correct.java
@@ -0,0 +1,16 @@
+public class Correct{
+    boolean b_check_count = false;
+    int count;
+    Correct(int count){
+        this.count = count;
+    }
+    
+    public boolean checkIsCorrectCount(){
+        if (this.count > 1 && this.count < 100000){
+            return b_check_count = true;
+        }
+        else{
+            return b_check_count = false;
+        }
+    }
+}
