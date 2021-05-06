724e86e1b517d7d56e8a5cc17e5155039e70e8e5
sldstrst <bog7673@gmail.com>
2021-05-06 16:10:48 +0300

    realese

--- Correct.java
+++ Correct.java
@@ -1,10 +1,12 @@
+package com.company;
+
 public class Correct{
     boolean b_check_count = false;
     int count;
     Correct(int count){
         this.count = count;
     }
-    
+
     public boolean checkIsCorrectCount(){
         if (this.count > 1 && this.count < 100000){
             return b_check_count = true;
