596d4bfe4c93ca1aa9b9aae669f011dcc10f44f0
sldstrst <bog7673@gmail.com>
2021-05-06 16:09:57 +0300

    realese

--- Person.java
+++ Person.java
@@ -1,27 +1,29 @@
+package com.company;
+
 import java.util.ArrayList;
 
 public class Person{
     String personal_name;
     int personal_id;
     String user_password;
-    
+
     Person(String name, String password){
         this.personal_name = name;
         this.user_password = password;
     }
-    
+
     public String getName(){
         return this.personal_name;
     }
-    
+
     public String getPassword(){
         return this.user_password;
     }
-    
+
     public void displayPerson(String name, String password){
         System.out.println(name + " " + password);
     }
-    
+
     public static void createPersonList(boolean bool, ArrayList<Person> persons){
         if (bool){
             Person person1 = new Person("bogdan", "123qwe");
