cfed0128550724cfaff5855c8c2563373d136fe3
sldstrst <bog7673@gmail.com>
2021-05-06 16:09:08 +0300

    release
    the system works as: log in (bogdan/kate + password: 123qwe/icecream) -> 1. Add dreams, 2. Delete dreams, 3.Show dreams, 4. Change user, 5. Leave

--- Main.java
+++ Main.java
@@ -1,33 +1,37 @@
+package com.company;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.InputStreamReader;
 
-public class Main{
-	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         boolean bool = false;
-        int count_persons = 3; 
+        int count_persons = 3;
         boolean btn_exit = false;
-        boolean flag_validate = false;
+        boolean flag_leave = false;
         String status_s_fill = "";
         String dream_fill = "";
-        ArrayList<Dreams> arr_dreams = new ArrayList<Dreams>();
+        boolean flag_validate = false;
         ArrayList<Person> arr_person = new ArrayList<Person>();
-      	Dreams.createDreamList(arr_dreams);
-        
+        //заполняем БД данными (имена, пароли)
         Correct corr_count = new Correct(count_persons);
         bool = corr_count.checkIsCorrectCount();
-        
         Person.createPersonList(bool, arr_person);
-        
+        ArrayList<Dreams> arr_dreams_u1 = new ArrayList<Dreams>();
+        ArrayList<Dreams> arr_dreams_u2 = new ArrayList<Dreams>();
         while(!flag_validate){
             System.out.print("Input a Name: ");
             String name = input.next();
-    		System.out.print("Input a password: ");
+            System.out.print("Input a password: ");
             String pass = input.next();
-            
+
+            //если вводимый пароль совпадает, то валидация проходит успешно
+            //(переходим на следующий этап - отображение данных)
             Validate validate = new Validate(name, pass);
             flag_validate = validate.validateCheck(arr_person);
-            if (flag_validate == false){
+            if (!flag_validate){
                 System.out.print("Input again? 1 - yes | 2 - no: ");
                 String cont = input.next();
                 if (cont.equals("2")){
@@ -35,47 +39,72 @@ public class Main{
                 }
                 else{
                     System.out.println("Input again..");
-                } 
+                }
+            }
+            flag_leave = flag_validate;
+
+            //Dreams.createDreamList(arr_dreams, name);
+            while (flag_leave){
+                System.out.println("1. Add to Dream");
+                System.out.println("2. Delete from Dream");
+                System.out.println("3. Show Dreams");
+                System.out.println("4. Relog");
+                System.out.println("5. Leave");
+                String choice = input.next();
+                switch (choice){
+                    case "1":
+                        Scanner scanner = new Scanner(new InputStreamReader(System.in));
+                        System.out.print("Input a dream");
+                        String dream_input = scanner.nextLine();
+                        System.out.print("Input a status");
+                        String s_status_input = scanner.nextLine();
+                        Dreams dream_user = new Dreams(dream_input, s_status_input, name);
+                        if (name.equals("kate")){
+                            dream_user.addToListDream(dream_input, s_status_input, arr_dreams_u1, name);
+                        }
+                        if (name.equals("bogdan")){
+                            dream_user.addToListDream(dream_input, s_status_input, arr_dreams_u2, name);
+                        }
+                        break;
+                    case "2":
+                        System.out.println("Del");
+                        Scanner scan = new Scanner(new InputStreamReader(System.in));
+                        System.out.print("Input a index del");
+                        String index_del = scan.nextLine();
+                        if (name.equals("bogdan")){
+                            Dreams.rmToListDream(arr_dreams_u2, index_del);
+                        }
+                        if (name.equals("kate")){
+                            Dreams.rmToListDream(arr_dreams_u1, index_del);
+                        }
+                        break;
+                    case "3":
+                        System.out.println("Show");
+                        if (name.equals("kate")) {
+                            for (int i = 0; i < arr_dreams_u1.size(); i++) {
+                                arr_dreams_u1.get(i).displayDreams(arr_dreams_u1.get(i), i);
+                            }
+                        }
+                        if (name.equals("bogdan")) {
+                            for (int i = 0; i < arr_dreams_u2.size(); i++) {
+                                arr_dreams_u2.get(i).displayDreams(arr_dreams_u2.get(i), i);
+                            }
+                        }
+                        System.out.println("\n");
+                        break;
+                    case "4":
+                        System.out.println("User change");
+                        flag_leave = false;
+                        flag_validate = false;
+                        break;
+                    case "5":
+                        System.out.println("Leave");
+                        flag_leave = false;
+                        flag_validate = true;
+                        break;
+                }
             }
         }
-        
-        while (flag_validate){
-           System.out.println("1. Add to Dream");
-           System.out.println("2. Delete from Dream");
-           System.out.println("3. Modify Dream");
-           System.out.println("4. Show Dreams");
-           System.out.println("5. Leave?");
-           String choice = input.next();
-           switch (choice){
-                case "1": 
-                    System.out.println("Add");
-               		System.out.print("Input a Dream: ");
-            		String dream_input = input.next();
-               		System.out.print("Input a Status: ");
-            		String s_status_input = input.next();
-                    Dreams.addToListDream(dream_input, s_status_input, arr_dreams);
-                    break;
-                case "2": 
-                    System.out.println("Del");
-                    break;
-                case "3": 
-                    System.out.println("Mod");
-                    break;
-                case "4": 
-                    System.out.println("Show");
-                    for (int i = 0; i < arr_dreams.size(); i++){
-                        arr_dreams.get(i).displayDreams(arr_dreams.get(i));
-                    }
-                    System.out.println("\n");
-                    break;
-                case "5": 
-                    System.out.println("Leave");
-                    flag_validate = false;
-                    break;
-           }
-        }
-        
-        
         input.close();
-	}
+    }
 }
